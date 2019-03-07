95ca493a5de30dbc792602a744a8280ae0bd2e60
sai <saikiranreddy524@gmail.com>
2019-03-07 13:34:26 -0500

    sai added

---
+++ bank-application/src/main/java/com/cybrilla/Saikiran.java
@@ -0,0 +1,13 @@
+package com.cybrilla;
+
+public class Saikiran {
+	public static void main(String args[]) {
+		int a  = 2;
+		int b = 0;
+		 int c; 
+		c = a+b;
+		System.out.println( c);
+		
+	}
+
+}
