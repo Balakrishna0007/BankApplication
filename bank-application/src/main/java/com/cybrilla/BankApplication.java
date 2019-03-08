61fd34e9ded61f01f164564fab78e094ead66f34
jobin <sebastienj341@gmail.com>
2019-03-07 23:51:19 -0500

    vhanged

--- bank-application/src/main/java/com/cybrilla/BankApplication.java
+++ bank-application/src/main/java/com/cybrilla/BankApplication.java
@@ -1,13 +1,14 @@
 package com.cybrilla;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class BankApplication 
 {
+	
 	public static void main(String[] args) 
 	{
+		System.out.println("hgyfygvutrftvybg");
 	}
 
 }
