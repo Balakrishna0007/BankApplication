02b53f5e3d2b62fe00b60bddeefb88bc2f43abbf
shalu2 <shalureddy24@gmail>
2019-03-08 23:02:15 -0500

    added done

--- bank-application/src/main/java/com/cybrilla/shallu1.java
+++ bank-application/src/main/java/com/cybrilla/shallu1.java
@@ -1,12 +1,13 @@
 package com.cybrilla;
 
-public class shallu1 {
+public class shallu1
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
+	public static void main(String[] args)
+	{
 		System.out.println("hi");
+		System.out.println("this is java world");
+		System.out.println("welcome to java");
 	}
 
 }
