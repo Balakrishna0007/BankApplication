2e59e698290a4586bc7f7bff7d264d1f2127c764
BalaKrishna <balakrishna@ictcrp.com>
2019-08-03 18:24:55 +0530

    dummy checkin for prudhvi demo

--- bank-application/src/main/java/com/cybrilla/BankApplication.java
+++ bank-application/src/main/java/com/cybrilla/BankApplication.java
@@ -1,6 +1,6 @@
 package com.cybrilla;
 
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankApplication 
@@ -9,6 +9,7 @@ public class BankApplication
 	public static void main(String[] args) 
 	{
 		System.out.println("hgyfygvutrftvybg");
+		System.out.println("My Name is Prudhvi V");
 	}
 
 }
---
+++ bank-application/src/main/java/com/cybrilla/PrudhviDemo.java
@@ -0,0 +1,11 @@
+package com.cybrilla;
+
+public class PrudhviDemo 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is dummy  program");
+		
+	}
+
+}
