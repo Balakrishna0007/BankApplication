44a01ae99c9d587e1133c4c64128edd3acf197b7
BalaKrishna <balakrishna@ictcrp.com>
2019-08-04 20:59:18 +0530

    This is the demo for QABatch10

--- bank-application/src/main/java/com/cybrilla/ABCD.java
+++ bank-application/src/main/java/com/cybrilla/ABCD.java
@@ -4,18 +4,7 @@ public class ABCD
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Changes");
-		System.out.println("Extra Changes");
-		System.out.println("another change!!");
-
-		System.out.println("Please Delete later !!");
-		System.out.println("hello");
-		System.out.println("Then Delete again");
-		
-		System.out.println("helloworld");
-		System.out.println("cool");
-
-		System.out.println("cool");
+		System.out.println("This are latest changes :) :) :)");
 
 	}
 
