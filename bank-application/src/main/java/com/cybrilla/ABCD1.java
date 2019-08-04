ebf695ebd163fd9a806e3d165bf95a0cb5fbc42c
Shanthi <pshanthiqa@gmail.com>
2019-08-04 15:49:08 -0400

    Prashanthi modified ABCD1 file

--- bank-application/src/main/java/com/cybrilla/ABCD1.java
+++ bank-application/src/main/java/com/cybrilla/ABCD1.java
@@ -2,5 +2,28 @@ package com.cybrilla;
 
 public class ABCD1 
 {
-
+  static  int i,j,k;
+   
+	public static void main(String[] args)
+	{
+		for (int i = 0; i < 5; i++) 
+		{
+            for (int j = 0; j < 5 - i; j++)
+            {
+                System.out.print(" ");
+            }
+            
+            for (int k = 0; k <= i; k++)
+            {
+                System.out.print("* ");
+                
+            }
+            System.out.println();	
+	
+	  }
+		
+	}
+	
+		
+	
 }
