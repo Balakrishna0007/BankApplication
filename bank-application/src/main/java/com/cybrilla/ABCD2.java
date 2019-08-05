05f5625c13074288d895b6f51ca333710cbb5883
prudhvi vempala <prudhvivempala@gmail.com>
2019-08-05 18:01:20 -0500

    dummy projects ABCD3

--- bank-application/src/main/java/com/cybrilla/ABCD2.java
+++ bank-application/src/main/java/com/cybrilla/ABCD2.java
@@ -3,5 +3,7 @@ package com.cybrilla;
 public class ABCD2 {
  public static void main(String[] args) {
 	System.out.println("hello world");
+	System.out.println("Changes done in ABCD2 project");
+
 }
 }
---
+++ bank-application/src/main/java/com/cybrilla/ABCD3.java
@@ -0,0 +1,11 @@
+package com.cybrilla;
+
+public class ABCD3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pratice project 2");
+		System.out.println("Commiting this project into github");
+
+	}
+
+}
