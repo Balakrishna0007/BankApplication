974a7d3084b4c462c13f98f8acc23574eb44940b
prudhvi vempala <prudhvivempala@gmail.com>
2019-09-26 15:45:27 -0500

    practicing how to do changes

---
+++ bank-application/src/main/java/SigmaITcrop/practice.java
@@ -0,0 +1,11 @@
+package SigmaITcrop;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello bro");
+
+	}
+
+}
--- bank-application/src/main/java/com/cybrilla/Dinesh.java
+++ bank-application/src/main/java/com/cybrilla/Dinesh.java
@@ -5,7 +5,7 @@ public class Dinesh {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello");
+		System.out.println("hello guru");
 		System.out.println("Changes");
 		System.out.println("Extra Changes");
 	}
