feb5b390abfbef2f59f8572c2b9496939c92e8c4
Ram_Charan <charan.maganti3@gmail.com>
2020-06-22 13:21:10 -0400

    Change made in the 4th commit

--- bank-application/src/main/java/com/bank/controllers/BankController.java
+++ bank-application/src/main/java/com/bank/controllers/BankController.java
@@ -28,7 +28,7 @@ public class BankController
 		//This is charan changes
 		//This is 2nd commit changes
 		//This 3rd commit
-		//This is 4th commit
+		//This is 4th commi
 		BankCustomer cus = service.createUser(customer);
 		return cus;
 	}
